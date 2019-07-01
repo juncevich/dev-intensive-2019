2574eb282cceec1118b34c40c38ea6b28fdf3205
Alex Juncevich <a.juncevich@gmail.com>
2019-07-01 22:39:32 +0300

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.model.User
+import ru.skillbranch.devintensive.model.UserView
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+fun User.toUserView(): UserView {
+
+    val nickName = Utils.transliteration("$firstName $lastVisit")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status =
+        if (lastVisit == null) "Еще ни разу не был" else if (isOnline) "online" else "Последний раз был ${lastVisit.hunanizeDiff()}"
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        nickName = nickName,
+        initials = avatar,
+        status = status
+    )
+}
+
+
